39c5b44c911703c91b4a7d2c05d1950b84cb575e
Badriddin <frozeshkh@gmail.com>
2022-04-07 23:03:26 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var arr = [[{ x:{ first: 20 } }], [{ y: { second: 35 } }], [{ h: { third: 44 } }]]
+var total = 0
+
+if (arr == total) {
+    console.log(total);
+}
+
+console.log(
+    arr[0][0].x.first + arr[1][0].y.second + arr[2][0].h.third
+    );
